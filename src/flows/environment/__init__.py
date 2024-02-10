56db757d6c3abf24fff1f6cef5ff8b70801099b3
inigohidalgo <inigohrey@gmail.com>
2024-02-11 00:09:09 +0100

    refactor

--- src/flows/environment/__init__.py
+++ src/flows/environment/__init__.py
@@ -1,9 +1,10 @@
 import prefect as pf
 import prefect.blocks as pfbs
+from pydantic import SecretStr
 
 
 @pf.flow
-def save_prefect_secret(secret_name: str, secret_value: str):
+def save_prefect_secret(secret_name: str, secret_value: SecretStr):
     pfbs.system.Secret(value=secret_value).save(secret_name)
 
 if __name__ == "__main__":
--- src/flows/examples/github_stats.py
+++ src/flows/examples/github_stats.py
@@ -10,7 +10,7 @@ def get_url(url: str, params: dict = None):
 
 
 @pf.flow
-def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
+def get_repo_info(repo_name: str = "inigohidalgo/kedro-ibis-dataset"):
     url = f"https://api.github.com/repos/{repo_name}"
     repo = pf.task(get_url)(url)
     logger = pf.get_run_logger()
@@ -19,9 +19,15 @@ def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
     logger.info(f"Forks 🍴 : %d", repo["forks_count"])
 
 
+def get_deployment(**kwargs):
+    deployment_kwargs = {
+        "name": "get-repo-info",
+        "interval": timedelta(days=7),
+    }
+    deployment_kwargs.update(kwargs)
+
+    return get_repo_info.to_deployment(**deployment_kwargs)
 
 if __name__ == "__main__":
-    # get_repo_info.serve(name="my-first-deployment")
-    my_repo_check = get_repo_info.to_deployment(interval=timedelta(minutes=30), name="my-repo-info", tags=["github", "api"], parameters={"repo_name": "inigohidalgo/kedro-ibis-dataset"})
-    pf.serve(my_repo_check)
+    pf.serve(get_deployment())
 
