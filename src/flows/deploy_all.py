8e35849b77ad4c98e47d4c68025fd081ce21f2f4
inigohidalgo <inigohrey@gmail.com>
2024-02-11 00:09:23 +0100

    feat: add example to deploy all

--- src/flows/deploy_all.py
+++ src/flows/deploy_all.py
@@ -1,8 +1,12 @@
+from datetime import timedelta
+
 import prefect as pf
 
 from environment import save_prefect_secret
+from examples import github_stats
 
 if __name__ == "__main__":
     pf.serve(
-        save_prefect_secret.to_deployment(name="save-prefect-secret")
+        save_prefect_secret.to_deployment(name="save-prefect-secret"),
+        github_stats.get_deployment(),
     )
