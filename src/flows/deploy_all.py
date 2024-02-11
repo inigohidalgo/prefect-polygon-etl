9cba414fbedd9ba3663521a6e24a0af88a4ee95b
inigohidalgo <inigohrey@gmail.com>
2024-02-11 01:14:23 +0100

    feat: add get aggregaes to all deployments

--- src/flows/deploy_all.py
+++ src/flows/deploy_all.py
@@ -4,9 +4,12 @@ import prefect as pf
 
 from environment import save_prefect_secret
 from examples import github_stats
+from polygon_elt import get_aggregates
+
 
 if __name__ == "__main__":
     pf.serve(
         save_prefect_secret.to_deployment(name="save-prefect-secret"),
         github_stats.get_deployment(),
+        get_aggregates.get_aggregates.to_deployment(name="get-aggregates"),
     )
