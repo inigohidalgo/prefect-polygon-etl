e5bdcc0e2c1f4f87e365012efc12cdf7b5ca2042
inigohidalgo <inigohrey@gmail.com>
2024-02-11 22:29:11 +0100

    feat: bronze output through polars

--- src/flows/polygon_elt/get_aggregates.py
+++ src/flows/polygon_elt/get_aggregates.py
@@ -7,7 +7,12 @@ import datetime
 from polygon import RESTClient
 import prefect.blocks.system as pfbs
 import s3fs
+import polars as pl
 
+from enum import Enum
+
+class BronzeConstants(Enum):
+    TIMESTAMP_INSERTION_COLUMN = "ingestion_timestamp"
 
 
 def get_daily_data(ticker, date_from, date_to, client: RESTClient):
@@ -22,7 +27,23 @@ raw_fs = s3fs.S3FileSystem(
 )
 
 
+pl_s3_delta_config = {
+    "endpoint": "http://localhost:9000",
+    "access_key": pfbs.Secret.load("minio-access-key").get(),
+    "secret_key": pfbs.Secret.load("minio-secret-key").get(),
+    "region": "us-east-1",
+    "allow_http": True,
+}
 
+
+def aggregations_to_bronze(aggregations, ticker):
+    return (
+        pl.DataFrame(aggregations)
+        .with_columns(
+            pl.lit(ticker).alias("ticker"),
+            pl.lit(datetime.datetime.utcnow()).alias(BronzeConstants.TIMESTAMP_INSERTION_COLUMN.value),
+            )
+        )
 
 @pf.flow
 def get_aggregates(ticker, date_from: datetime.date, date_to: datetime.date, container: str = "etl/polygon/raw"):
@@ -30,11 +51,13 @@ def get_aggregates(ticker, date_from: datetime.date, date_to: datetime.date, con
     client = get_polygon_client()
     daily_aggs = get_daily_data(ticker, date_from, date_to, client)
     logger.info(f"Got {len(daily_aggs)} daily aggregates for {ticker} from {date_from} to {date_to}")
-    daily_agggs_dicts = [agg.__dict__ for agg in daily_aggs]
-    raw_json_path = f"{container}/{ticker}/{date_from}__{date_to}.json"
-    logger.info(f"Writing daily aggregates to {raw_json_path}")
-    with raw_fs.open(raw_json_path, "w") as f:
-        json.dump(daily_agggs_dicts, f)
+    
+    daily_aggs_pl = aggregations_to_bronze(daily_aggs, ticker)
+
+    daily_aggs_pl.write_parquet(
+        f"s3://{container}/{ticker}/daily_aggs/{date_from}_{date_to}.parquet",
+        **pl_s3_delta_config
+    )
 
 
 if __name__=="__main__":
