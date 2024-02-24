d0cd5bfc049a4ab4f96ae1ab997efb34ceda2a2d
inigohidalgo <inigohrey@gmail.com>
2024-02-24 01:24:06 +0100

    revert: remove upsert from tests

--- delta-rs-etl/tests/__init__.py
+++ delta-rs-etl/tests/__init__.py
@@ -1,16 +1,3 @@
 from deltalake import DeltaTable
 
-def upsert(new_data, target_table: DeltaTable, primary_key: list[str]):
-    predicate = " AND ".join([f"target.{col} = source.{col}" for col in primary_key])
-    return (
-        target_table
-        .merge(
-            source=new_data,
-            predicate=predicate,
-            source_alias="source",
-            target_alias="target"
-        )
-        .when_matched_update_all()
-        .when_not_matched_insert_all()
-        .execute()
-    )
+
