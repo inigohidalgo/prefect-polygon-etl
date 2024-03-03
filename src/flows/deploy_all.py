27fa17cf6a34f734e336fa203bab7a786731331f
inigohidalgo <inigohrey@gmail.com>
2024-03-03 12:48:05 +0100

    feat: change deploy_all to modules

--- src/flows/deploy_all.py
+++ src/flows/deploy_all.py
@@ -4,17 +4,17 @@ from prefect.client.schemas.schedules import IntervalSchedule
 import prefect as pf
 
 from environment import save_prefect_secret, save_minio_credentials
-from polygon_elt import get_aggregates
+from polygon_elt import bronze, silver
 
 
 if __name__ == "__main__":
     pf.serve(
         save_prefect_secret.to_deployment(name="save-prefect-secret"),
         save_minio_credentials.to_deployment(name="save-minio-credentials"),
-        get_aggregates.aggregates_raw_to_silver.to_deployment(
+        silver.aggregates_raw_to_silver.to_deployment(
             name="aggregates-raw-to-silver"
         ),
-        get_aggregates.get_aggregates_from_preconfigured_list_of_tickers_raw_to_bronze.to_deployment(
+        bronze.get_aggregates_from_preconfigured_list_of_tickers_raw_to_bronze.to_deployment(
             name="daily-aggregates-raw-to-bronze-multiple-tickers",
             interval=timedelta(days=1),
             ),
