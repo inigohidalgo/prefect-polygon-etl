5cd622d1b9c5314984d114c96ce8f1cf57da80ba
inigohidalgo <inigohrey@gmail.com>
2024-03-03 17:41:13 +0100

    refactor: move configure flows into module

--- src/polygon_etl/flows/deploy_all.py
+++ src/polygon_etl/flows/deploy_all.py
@@ -3,20 +3,16 @@ from prefect.client.schemas.schedules import IntervalSchedule
 
 import prefect as pf
 
-from environment import save_prefect_secret, save_minio_credentials
-from polygon_elt import bronze, silver
+from polygon_etl.flows import bronze, silver, configure
 
 
 if __name__ == "__main__":
     pf.serve(
-        save_prefect_secret.to_deployment(name="save-prefect-secret"),
-        save_minio_credentials.to_deployment(name="save-minio-credentials"),
-        silver.aggregates_raw_to_silver.to_deployment(
-            name="aggregates-raw-to-silver"
-        ),
+        configure.save_prefect_secret.to_deployment(name="save-prefect-secret"),
+        configure.save_minio_credentials.to_deployment(name="save-minio-credentials"),
+        silver.aggregates_raw_to_silver.to_deployment(name="aggregates-raw-to-silver"),
         bronze.get_aggregates_from_preconfigured_list_of_tickers_raw_to_bronze.to_deployment(
             name="daily-aggregates-raw-to-bronze-multiple-tickers",
             interval=timedelta(days=1),
-            ),
-        )
-
+        ),
+    )
