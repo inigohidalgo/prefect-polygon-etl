ff0ac924aa82e8613a632c29c1e7de520a1045ea
inigohidalgo <inigohrey@gmail.com>
2024-03-04 12:49:40 +0100

    fix: save bronze under bronze instead of raw
    use Constants Enum to set bucket

--- src/polygon_etl/flows/bronze.py
+++ src/polygon_etl/flows/bronze.py
@@ -14,12 +14,16 @@ from polygon_etl.config.storage import MinIOCredentials, MINIO_CREDENTIAL_SECRET
 
 class Constants(Enum):
     TIMESTAMP_INSERTION_COLUMN = "ingestion_timestamp"
+    BUCKET = "etl/polygon/bronze"
 
 
 def bronze_query_parameters_to_path(
-    ticker, date_from, date_to, container="etl/polygon/raw"
+    ticker,
+    date_from,
+    date_to,
+    container=Constants.BUCKET.value,
 ):
-    return f"s3://{container}/{ticker}/daily_aggs/{date_from}_{date_to}.parquet"
+    return f"s3://{container}/stocks/aggs/daily/{ticker}/{date_from}_{date_to}.parquet"
 
 
 def get_daily_data(
@@ -46,7 +50,6 @@ def aggregates_load_raw_and_transform_to_bronze(
     ticker,
     date_from: datetime.date,
     date_to: datetime.date,
-    container: str = "etl/polygon/raw",
 ):
     logger = pf.get_run_logger()
     client = get_polygon_client()
@@ -81,7 +84,12 @@ def aggregates_save_bronze(
 
 
 @pf.task
-def aggregates_load_bronze(ticker, date_from, date_to, container="etl/polygon/raw"):
+def aggregates_load_bronze(
+    ticker,
+    date_from,
+    date_to,
+    container=Constants.BUCKET.value,
+):
     minio_credentials = MinIOCredentials.load(MINIO_CREDENTIAL_SECRET_KEY)
 
     return pl.scan_parquet(
@@ -95,21 +103,24 @@ def aggregates_raw_to_bronze(
     ticker: str,
     date_from: datetime.date,
     date_to: datetime.date,
-    container: str = "etl/polygon/raw",
+    container: str = Constants.BUCKET.value,
 ):
     pf.get_run_logger().info(
         f"Saving BRONZE daily aggregates for {ticker} from {date_from} to {date_to}"
     )
-    daily_aggs = aggregates_load_raw_and_transform_to_bronze(
-        ticker, date_from, date_to, container
-    )
+    daily_aggs = aggregates_load_raw_and_transform_to_bronze(ticker, date_from, date_to)
     aggregates_save_bronze(daily_aggs, ticker, date_from, date_to, container)
     return daily_aggs
 
 
 @pf.flow
-def aggregates_from_list_of_tickers_raw_to_bronze(tickers: list[str], date_from: datetime.date, date_to: datetime.date, container: str = "etl/polygon/raw", api_calls_per_second: int = 1):
-    
+def aggregates_from_list_of_tickers_raw_to_bronze(
+    tickers: list[str],
+    date_from: datetime.date,
+    date_to: datetime.date,
+    container: str = Constants.BUCKET.value,
+    continue_on_error: bool = False,
+):
     for ticker in tickers:
         try:
             aggregates_raw_to_bronze(ticker, date_from, date_to, container)
@@ -124,14 +135,13 @@ def aggregates_from_list_of_tickers_raw_to_bronze(tickers: list[str], date_from:
 
     pf.get_run_logger().info(f"Loaded {len(tickers)} tickers successfully")
 
+
 @pf.flow
 def get_aggregates_from_preconfigured_list_of_tickers_raw_to_bronze(
     tickers: Optional[list[str]] = None,
     n_days_back: int = 30,
-    container: Optional[str] = "etl/polygon/raw",
-    api_calls_per_second: Optional[int] = 1,
+    container: Optional[str] = Constants.BUCKET.value,
 ):
-    
     tickers = tickers or pfbs.JSON.load("polygon-daily-aggregates-tickers").value
     date_to = datetime.date.today() - datetime.timedelta(days=1)
     date_from = date_to - datetime.timedelta(days=n_days_back)
